4e5b07eddbf64d179e5d1db2176d4fd08e4fb5b3
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-03-22 15:43:55 +0900

    end

--- UITextField Delegate Test/ViewController.swift
+++ UITextField Delegate Test/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblH: UILabel!
+    @IBOutlet weak var txtF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtF.placeholder = "입력을 하세요"
+        txtF.clearButtonMode = UITextFieldViewMode.whileEditing
+        txtF.borderStyle = UITextBorderStyle.line
     }
 
+    
+    @IBAction func btn(_ sender: Any) {
+        lblH.text = "Hello " + txtF.text!
+        txtF.text = ""
+        // 키패드를 내림
+        txtF.resignFirstResponder()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+        //background의 View를 누르면 키패드가 내려감
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        txtF.resignFirstResponder()
+        //txtF.endEditing(false)
+    }
 }
 
